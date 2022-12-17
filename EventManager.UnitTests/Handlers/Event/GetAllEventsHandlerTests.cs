504c78ab6960ce845795bf1a6f6c5c4317e81396
Reza Ghasemi <reza.ghasemi@amily.de>
2022-12-18 00:09:47 +0100

    unit tests

---
+++ EventManager.UnitTests/Handlers/Event/GetAllEventsHandlerTests.cs
@@ -0,0 +1,61 @@
+﻿using EventManager.Core.Application.Event.GetAllEvents;
+using EventManager.Core.Domain.Contracts.Repository;
+using EventManager.Core.Domain.Entities.User;
+using EventManager.Core.Domain.ValueObjects;
+using Moq;
+using Xunit;
+
+namespace EventManager.UnitTests.Handlers.Event
+{
+    public class GetAllEventsHandlerTests
+    {
+        private readonly Mock<IEventRepository> _eventRepositoryMock;
+        private readonly GetAllEventsHandler _handler;
+        private readonly GetAllEventsQuery _request;
+
+        public GetAllEventsHandlerTests()
+        {
+            _eventRepositoryMock = new Mock<IEventRepository>();
+            _handler = new GetAllEventsHandler(_eventRepositoryMock.Object);
+            _request = new GetAllEventsQuery();
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsSuccessResult_WhenRepositoryReturnsEvents()
+        {
+            // Arrange
+            var userName = "TestUserName";
+            var user = User.CreateUser(Guid.NewGuid(), "test user", "TestUserName", userName, Email.CreateIfNotEmpty("test@gmail.com"));
+            _eventRepositoryMock.Setup(repo => repo.GetAllEventsAsync(It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new List<Core.Domain.Entities.Event.Event> {
+                   
+                    Core.Domain.Entities.Event.Event.CreatEvent(Guid.NewGuid(), "Test Event", "This is a test event", "Test location",
+                    DateTime.Now, DateTime.Now.AddHours(1), user),
+                    Core.Domain.Entities.Event.Event.CreatEvent(Guid.NewGuid(), "Test Event2", "This is a test event2", "Test location2",
+                        DateTime.Now, DateTime.Now.AddHours(1), user),
+                });
+
+            // Act
+            var result = await _handler.Handle(_request, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.NotEmpty(result.Result.GetEventResults);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsFailureResult_WhenRepositoryThrowsException()
+        {
+            // Arrange
+            _eventRepositoryMock.Setup(repo => repo.GetAllEventsAsync(It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("Test exception"));
+
+            // Act
+            var result = await _handler.Handle(_request, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Test exception", result.ErrorMessage);
+        }
+    }
+}
---
+++ EventManager.UnitTests/Handlers/Event/GetEventHandlerTests.cs
@@ -0,0 +1,57 @@
+﻿using EventManager.Core.Application.Event.GetEvent;
+using EventManager.Core.Domain.Contracts.Repository;
+using EventManager.Core.Domain.Entities.User;
+using EventManager.Core.Domain.ValueObjects;
+using Moq;
+using Xunit;
+
+namespace EventManager.UnitTests.Handlers.Event
+{
+    public class GetEventHandlerTests
+    {
+        private readonly Mock<IEventRepository> _eventRepositoryMock;
+        private readonly GetEventHandler _handler;
+        private readonly GetEventQuery _request;
+
+        public GetEventHandlerTests()
+        {
+            _eventRepositoryMock = new Mock<IEventRepository>();
+            _handler = new GetEventHandler(_eventRepositoryMock.Object);
+            _request = new GetEventQuery { EventId = Guid.NewGuid() };
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsSuccessResult_WhenRepositoryReturnsEvent()
+        {
+            // Arrange
+            var userName = "TestUserName";
+            var user = User.CreateUser(Guid.NewGuid(), "test user", "TestUserName", userName, Email.CreateIfNotEmpty("test@gmail.com"));
+
+            _eventRepositoryMock.Setup(repo => repo.GetEventByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(Core.Domain.Entities.Event.Event.CreatEvent(Guid.NewGuid(), "Test Event", "This is a test event", "Test location",
+                    DateTime.Now, DateTime.Now.AddHours(1), user));
+
+            // Act
+            var result = await _handler.Handle(_request, CancellationToken.None);
+
+            // Assert
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Result);
+        }
+
+        [Fact]
+        public async Task Handle_ReturnsFailureResult_WhenRepositoryThrowsException()
+        {
+            // Arrange
+            _eventRepositoryMock.Setup(repo => repo.GetEventByIdAsync(It.IsAny<Guid>(), It.IsAny<CancellationToken>()))
+                .ThrowsAsync(new Exception("Test exception"));
+
+            // Act
+            var result = await _handler.Handle(_request, CancellationToken.None);
+
+            // Assert
+            Assert.False(result.IsSuccess);
+            Assert.Equal("Test exception", result.ErrorMessage);
+        }
+    }
+}
